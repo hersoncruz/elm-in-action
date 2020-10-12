c837559d8d805769960efad319654e06af93fe91
Herson Cruz <herson@multisistemas.com.sv>
2020-10-12 08:32:02 -0600

    click working

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,7 +1,9 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Browser
+import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 urlPrefix =
   "http://elm-in-action.com/"
@@ -10,8 +12,7 @@ view model =
   div [ class "content" ]
       [ h1 [] [ text "Photo Groove" ]
       , div [ id "thumbnails" ] 
-        (List.map
-          (viewThumbnail model.selectedUrl)
+        (List.map (viewThumbnail model.selectedUrl)
           model.photos
         )
       , img
@@ -25,6 +26,7 @@ viewThumbnail selectedUrl thumb =
     img
       [ src (urlPrefix ++ thumb.url)
       , classList [ ("selected", selectedUrl == thumb.url ) ]
+      , onClick { description = "ClickedPhoto", data = thumb.url }
       ]
       []
 
@@ -34,9 +36,20 @@ initialModel =
     , { url = "2.jpeg" }
     , { url = "3.jpeg" }
     ]
-  , selectedUrl = "1.jpeg"
+  , selectedUrl = "3.jpeg"
   }
 
+update msg model =
+  if msg.description == "ClickedPhoto" then
+    { model | selectedUrl = msg.data }
+  else
+    model
+
 main =
-  view initialModel
+  Browser.sandbox
+    {
+      init = initialModel
+    , view = view
+    , update = update
+    }
 
