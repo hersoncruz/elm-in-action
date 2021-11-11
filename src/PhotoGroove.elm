93583bd15062322d1213eccc93fa8c49402abe25
Herson Cruz <herson@datolab.com>
2021-11-10 19:53:02 -0600

    progress

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,55 +1,92 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+
+urlPrefix : String
 urlPrefix =
-  "http://elm-in-action.com/"
+    "http://elm-in-action.com/"
 
+
+type alias Msg =
+    { description : String, data : String }
+
+
+view : Model -> Html Msg
 view model =
-  div [ class "content" ]
-      [ h1 [] [ text "Photo Groove" ]
-      , div [ id "thumbnails" ] 
-        (List.map (viewThumbnail model.selectedUrl)
-          model.photos
-        )
-      , img
-        [ class "large"
-        , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me!" ]
+        , div [ id "thumbnails" ]
+            (List.map (viewThumbnail model.selectedUrl)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
-        []
-      ]
 
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
-      [ src (urlPrefix ++ thumb.url)
-      , classList [ ("selected", selectedUrl == thumb.url ) ]
-      , onClick { description = "ClickedPhoto", data = thumb.url }
-      ]
-      []
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
 
-initialModel =
-  { photos = 
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
-  , selectedUrl = "3.jpeg"
-  }
 
-update msg model =
-  if msg.description == "ClickedPhoto" then
-    { model | selectedUrl = msg.data }
-  else
-    model
+type alias Photo =
+    { url : String }
 
-main =
-  Browser.sandbox
-    {
-      init = initialModel
-    , view = view
-    , update = update
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
     }
 
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "3.jpeg"
+    }
+
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
